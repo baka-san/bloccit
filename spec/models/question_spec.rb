540f9f0dac3977e6cc772aaf9ab8242c31a40da8
Kasey Markham <kaseycm@gmail.com>
2017-04-08 21:16:11 -0400

    Added Question and Answer models

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Question, type: :model do
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
+
+  describe "attributes" do
+   it "has title and body attributes" do
+     expect(question).to have_attributes(title: "New Question Title", body: "New Question Body")
+   end
+
+   it "should respond to resolved" do
+     expect(question).to respond_to(:resolved)
+   end
+
+  end
+end
