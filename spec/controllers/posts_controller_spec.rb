348d1613120e767baebd83e83a7b60bf70a1d7b6
Kasey Markham <kaseycm@gmail.com>
2017-04-13 15:43:31 -0400

    Questions added

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -43,13 +43,11 @@ describe "GET new" do
       expect(response).to have_http_status(:success)
     end
 
-# #2
     it "renders the #new view" do
       get :new
       expect(response).to render_template :new
     end
 
-# #3
     it "instantiates @post" do
       get :new
       expect(assigns(:post)).not_to be_nil
@@ -57,18 +55,16 @@ describe "GET new" do
   end
 
   describe "POST create" do
-# #4
+
     it "increases the number of Post by 1" do
       expect{post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}}.to change(Post,:count).by(1)
     end
 
-# #5
     it "assigns the new post to @post" do
       post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
       expect(assigns(:post)).to eq Post.last
     end
 
-# #6
     it "redirects to the new post" do
       post :create, post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
       expect(response).to redirect_to Post.last
